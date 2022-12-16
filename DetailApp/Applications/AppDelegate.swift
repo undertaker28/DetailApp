b32c42701f3d536ea11a2fbd78eb921ea7c98cb1
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-12-16 20:08:35 +0300

    Delete Sotoryboard and add project structure

---
+++ DetailApp/Applications/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DetailApp
+//
+//  Created by Pavel on 16.12.22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
